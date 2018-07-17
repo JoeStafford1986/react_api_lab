3969b5a5ba54b7f0ea5f128672dfa98181d84b21
Joe Stafford <joestafford1986@gmail.com>
2018-07-17 13:59:58 +0100

    initial commit

---
+++ src/containers/CharacterContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class CharacterContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Harry Potter Characters</h2>
+
+      </div>
+    );
+  }
+}
+
+export default CharacterContainer;
